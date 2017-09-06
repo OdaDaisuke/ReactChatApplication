3880f0f8b05cf63ccbe0a9b065c0b12aa4e2ab5e
DaisukeOda <hinopon25@gmail.com>
2017-09-06 11:29:19 +0900

    Headerをリファクタリング

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,12 @@
-import React, { Component } from "react"
+import React from "react"
 
-export default class Header extends Component {
-  render() {
-    return (
-      <header className="layout">
-        <a href="/" className="brand">
-          <img src="http://placehold.jp/20/525252/ffffff/150x50.png?text=LOGO" alt="LOGO" />
-        </a>
-        MENU
-      </header>
-    )
-  }
-}
+const Header = () => (
+  <header className="layout">
+    <a href="/" className="brand">
+      <img src="http://placehold.jp/20/525252/ffffff/150x50.png?text=LOGO" alt="LOGO" />
+    </a>
+    MENU
+  </header>
+)
+
+export default Header
