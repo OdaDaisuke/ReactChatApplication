4877c0465eeccba368175e29b3bee16a3397f358
DaisukeOda <hinopon25@gmail.com>
2017-09-06 09:47:03 +0900

    LOGOのurl変更

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export default class Header extends Component {
     return (
       <header className="layout">
         <a href="/" className="brand">
-          <img src="logo.jpg" alt="LOGO" />
+          <img src="http://placehold.jp/20/525252/ffffff/150x50.png?text=LOGO" alt="LOGO" />
         </a>
         MENU
       </header>
