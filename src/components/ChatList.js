189577874904ce2d5ef1d341ed5806a10eb5291a
DaisukeOda <hinopon25@gmail.com>
2017-09-06 12:17:17 +0900

    修正

--- src/components/ChatList.js
+++ src/components/ChatList.js
@@ -8,7 +8,7 @@ const ChatList = ({chats, currentOpponent, myId}) => (
         _chat.send_to === myId) {
         return (
           <div key={_chat.uuid} className="message message--receive">
-            <span>@{_chat.from_id}</span>
+            <span>@{_chat.handle_name}</span>
             <p>{_chat.body}</p>
           </div>
         )
